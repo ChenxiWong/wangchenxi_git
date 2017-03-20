9655887993bede7abacc724e580eb1d0c68b499e
wangchenxi <chinawangchenxi@gmail.com>
2017-03-20 13:08:18 +0800

    create and use freemind to discribe my life

---
+++ Idea_project/freemind/我的人生经历.mm
@@ -0,0 +1,29 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1489985544202" ID="ID_973524381" MODIFIED="1489985567613" TEXT="&#x6211;&#x7684;&#x4eba;&#x751f;&#x7ecf;&#x5386;">
+<node CREATED="1489985581271" ID="ID_383831649" MODIFIED="1489985715774" POSITION="right" TEXT="&#x4e0a;&#x5b66;&#x9636;&#x6bb5;">
+<icon BUILTIN="knotify"/>
+<node CREATED="1489985930438" ID="ID_1343084130" MODIFIED="1489985971838" TEXT="&#x81ea;&#x52a8;&#x5316;">
+<icon BUILTIN="password"/>
+</node>
+<node CREATED="1489985992929" ID="ID_194805945" MODIFIED="1489986015843" TEXT="&#x8bfe;&#x5916;&#x5b66;&#x4e60;">
+<icon BUILTIN="wizard"/>
+</node>
+</node>
+<node CREATED="1489985600621" ID="ID_1739482680" MODIFIED="1489985696908" POSITION="left" TEXT="&#x5de5;&#x4f5c;&#x9636;&#x6bb5;">
+<icon BUILTIN="folder"/>
+<node CREATED="1489985785065" ID="ID_746666636" MODIFIED="1489986172158" TEXT="C++&#x8f6f;&#x4ef6;&#x5f00;&#x53d1;&#x5de5;&#x7a0b;&#x5e08;">
+<icon BUILTIN="penguin"/>
+</node>
+<node CREATED="1489985739430" ID="ID_708756592" MODIFIED="1489985911005" TEXT="&#x4f01;&#x4e1a;&#x7f51;&#x7edc;&#x7ba1;&#x7406;&#x5458;">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1489986086587" ID="ID_1365346179" MODIFIED="1489986115651" TEXT="&#x4eea;&#x5668;&#x4eea;&#x8868;&#x7ef4;&#x62a4;">
+<icon BUILTIN="freemind_butterfly"/>
+</node>
+</node>
+<node CREATED="1489985651475" ID="ID_183538366" MODIFIED="1489985727013" POSITION="right" TEXT="&#x5176;&#x4ed6;">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</map>
