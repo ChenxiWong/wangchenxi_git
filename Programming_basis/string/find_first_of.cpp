959e8775da339abe1ee4ea08cb2e4b29997d17bd
wangchenxi <chinawangchenxi@gmail.com>
2017-03-30 18:19:27 +0800

    string

---
+++ Programming_basis/string/find_first_of.cpp
@@ -0,0 +1,14 @@
+// Last Update:2017-03-30 11:45:07
+/**
+ * @file find_first_of.cpp
+ * @brief 
+ * @author wangchenxi
+ * @version 0.1.00
+ * @date 2017-03-30
+ */
+#include<string>
+using namespace std;
+int main()
+{
+    string tmp_str = "./a.out -1 123 -d "
+}
