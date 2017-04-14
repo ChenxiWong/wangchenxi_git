1d7001f652426f4651ec4bbe1a6a92c4832f2352
wangchenxi <chinawangchenxi@gmail.com>
2017-04-14 15:06:20 +0800

    hash_map in C++ and ntfs-3g in linux

---
+++ Programming_basis/hash_map/hash_map.cpp
@@ -0,0 +1,38 @@
+// Last Update:2017-04-13 14:35:00
+/**
+ * @file hash_map.cpp
+ * @brief 
+ * @author wangchenxi
+ * @version 0.1.00
+ * @date 2017-04-13
+ */
+#include <ext/hash_map>
+#include<string>
+#include<iostream>
+#include<time.h>
+using namespace __gnu_cxx;
+using std::string;
+using std::pair;
+namespace __gnu_cxx
+{
+    template<> struct hash<string>
+    {
+        size_t operator()(const string& s) const
+        {
+            return __stl_hash_string(s.c_str());
+        }
+    };
+}
+
+int main()
+{
+
+    hash_map<string, int> hm;
+    hm["hello"] = 100 + hm["hello"];
+    std::cout<<hm["hello"]<<std::endl;
+
+    hash_map<string, pair<unsigned long long , time_t>> hm1;
+    hm1["hello"]
+    /* 其它使用hash_map的代码 */
+
+}
