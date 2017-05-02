6ecf33acef4cfa0c9845f77608e49ac321deab38
wangchenxi <chinawangchenxi@gmail.com>
2017-05-02 14:42:54 +0800

    rename test

---
+++ Programming_basis/rename/rename.cpp
@@ -0,0 +1,15 @@
+// Last Update:2017-05-02 14:17:01
+/**
+ * @file rename.cpp
+ * @brief 
+ * @author wangchenxi
+ * @version 0.1.00
+ * @date 2017-05-02
+ */
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    rename(argv[1], argv[2]);
+    return 0;
+}
